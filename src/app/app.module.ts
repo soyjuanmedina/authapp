1f6c39a7185196bbb88bf89516249d49cc22241c
Juan Medina <soyjuanmedina@gmail.com>
2018-01-26 17:56:27 +0100

    From Linux home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app.routes
+import { AppRoutingModule } from './app.routes';
 
 
 import { AppComponent } from './app.component';
