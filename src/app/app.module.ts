d559241fb2d95475ca3c1e3c70c8560b5e1b88cd
Juan Medina <soyjuanmedina@gmail.com>
2018-01-26 19:35:36 +0100

    Add simple auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
 import { AppRoutingModule } from './app.routes';
 
+//Services
+import { AuthService } from './services/auth.service';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -23,7 +27,9 @@ import { ProtegidaComponent } from './components/protegida/protegida.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
